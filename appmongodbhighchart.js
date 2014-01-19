70156efd63c385efc9ce9568e1edb672f86e9f98
Lokesh Gyanchandani <lokesh.sgc@gmail.com>
2014-01-19 05:27:57 -0500

    highchart.html

--- appmongodbhighchart.js
+++ appmongodbhighchart.js
@@ -9,6 +9,20 @@ var db = require("mongojs").connect(databaseUrl, collections);
 
 var app = express();
 
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./highchart.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(80);
+});
 
 
 // Config
